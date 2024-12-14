94a2c66d66b3da1affeaa4c81d20470fd5eb6999
CNCTD <74155252+CNCTD@users.noreply.github.com>
2024-12-13 20:22:15 -0500

    Update bstest.js

--- bstest.js
+++ bstest.js
@@ -24,23 +24,27 @@ window.addEventListener('load', () => {
     }
 });
 
-window.screen.orientation.addEventListener("change", function(e) {
-    const orientation = window.screen.orientation.angle; // Get the current orientation angle
+if (/Mobi|Android|iPhone|iPad|iPod|Windows Phone/i.test(navigator.userAgent)) {
+    window.screen.orientation.addEventListener("change", function(e) {
+        const orientation = window.screen.orientation.angle; // Get the current orientation angle
 
-    if (orientation === 0) {
-        console.log('Portrait Mode');
-        starMap.style.visibility = "hidden";
-        scrambleText(infoBox, `Please rotate your screen.`);
-    } else if (orientation === 90 || orientation === -90) {
-        scrambleText(infoBox, `Thank you.`);
-        setTimeout(() => location.reload(), 2000); // Fixed the timeout syntax
-    } else {
-        console.log('Unknown Orientation');
-    }
+        if (orientation === 0) {
+            console.log('Portrait Mode');
+            starMap.style.visibility = "hidden";
+            scrambleText(infoBox, `Please rotate your screen.`);
+        } else if (orientation === 90 || orientation === -90) {
+            scrambleText(infoBox, `Thank you.`);
+            setTimeout(() => location.reload(), 2000); // Fixed the timeout syntax
+        } else {
+            console.log('Unknown Orientation');
+        }
 
-    // This function will be called when the screen orientation changes.
-    console.log("Orientation changed to:", window.screen.orientation.type);
-});
+        // This function will be called when the screen orientation changes.
+        console.log("Orientation changed to:", window.screen.orientation.type);
+    });
+} else {
+    console.log('Not a mobile device, ignoring orientation change events.');
+}
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
